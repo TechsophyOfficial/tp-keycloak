e97539dbd40f251ea56259a9287cf48a864dd0dd
Sanjay Singh <sanjay.s@techsophy.com>
2023-05-18 16:39:59 +0530

    Register User SPI jar and Source

--- pluginSrc/ts-awgment-user-register-spi/src/main/java/com/techsophy/awgment/utils/RestAPIUtils.java
+++ pluginSrc/ts-awgment-user-register-spi/src/main/java/com/techsophy/awgment/utils/RestAPIUtils.java
@@ -57,64 +57,6 @@ public class RestAPIUtils {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-//        try {
-//
-//            String stubsApiBaseUri = "http://localhost:8082/user";
-//
-//            HttpClient client = HttpClients.createDefault();
-//
-//            URIBuilder builder = new URIBuilder(stubsApiBaseUri);
-//            builder.addParameter("eventType", eventType);
-//            builder.addParameter("userName", userName);
-//            builder.addParameter("userId", userId);
-//            builder.addParameter("realm", realm);
-//            builder.addParameter("identityProvider", identityProvider);
-//            builder.addParameter("clientId", clientId);
-//            String listStubsUri = builder.build().toString();
-//            HttpGet getStubMethod = new HttpGet(listStubsUri);
-//            HttpResponse getStubResponse = client.execute(getStubMethod);
-//            int getStubStatusCode = getStubResponse.getStatusLine()
-//                    .getStatusCode();
-//            if (getStubStatusCode < 200 || getStubStatusCode >= 300) {
-//                // Handle non-2xx status code
-//                return;
-//            }
-//            String responseBody = EntityUtils
-//                    .toString(getStubResponse.getEntity());
-//            // Assuming only one stub matches
-//            String stubRelativeUri = XPathFactory
-//                    .newInstance()
-//                    .newXPath()
-//                    .evaluate("/stubs/stub/@href",
-//                            new InputSource(new StringReader(responseBody)));
-//            String stubAbsoluteUri = new URI(stubsApiBaseUri).resolve(
-//                    stubRelativeUri).toString();
-//
-//            HttpPost startStubMethod = new HttpPost(stubAbsoluteUri);
-//            ContentType contentType = ContentType.APPLICATION_XML
-//                    .withCharset("utf-8");
-//            String data = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-//                    + "<start-stub />";
-//            startStubMethod.setEntity(new ByteArrayEntity(data
-//                    .getBytes(contentType.getCharset()), contentType));
-//            HttpResponse startStubResponse = client.execute(startStubMethod);
-//            int startStubStatusCode = startStubResponse.getStatusLine()
-//                    .getStatusCode();
-//            if (startStubStatusCode < 200 || startStubStatusCode >= 300) {
-//                // Handle non-2xx status code
-//                return;
-//            }
-//            // If you want to check the status of the stub that is starting, you
-//            // can use the response data to get the stub instance URI and poll it
-//            // for updates
-//            System.out.println(startStubStatusCode);
-//            String startStubResponseBody = EntityUtils.toString(startStubResponse
-//                    .getEntity());
-//            System.out.println(startStubResponseBody);
-//
-//        } catch (URISyntaxException | IOException | XPathExpressionException e) {
-//            // Handle errors
-//        }
     }
 
 }
